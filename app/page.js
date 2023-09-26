b2fdb6f544865ef0b497cc4abe7071226f5a5ac7
Dann <danilo.pena@ascential.com>
2023-09-26 17:48:42 -0500

    no content yet

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
-      <nav className="bg-gray-900 text-white p-4">
-      <div className="container mx-auto">
-        <h1 className="text-2xl font-semibold">Coffee Stores</h1>
-      </div>
-    </nav>
-  )
+      <h1
+      >
+      hello world!
+      </h1>
+        )
 }
