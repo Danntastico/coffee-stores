dfc1a6ddf8d0e925c82798b1020569d3443b7936
Dann <danilo.pena@ascential.com>
2023-09-26 18:16:04 -0500

    Correct tab indentation

--- app/layout.js
+++ app/layout.js
@@ -12,11 +12,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <nav className="bg-gray-900 text-white p-4">
-      <div className="container mx-auto">
-        <h1 className="text-2xl font-semibold">Coffee Stores</h1>
-      </div>
-    </nav>
+        <nav className="bg-gray-900 text-white p-4">
+          <div className="container mx-auto">
+            <h1 className="text-2xl font-semibold">Coffee Stores</h1>
+          </div>
+        </nav>
         {children}
       </body>
     </html>
