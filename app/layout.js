72d1e392b598f37ef106d3fc6f13fe890b442076
Dann <danilo.pena@ascential.com>
2023-09-27 18:23:17 -0500

    initial JSX structure

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Roboto } from 'next/font/google'
+import { Roboto_Flex } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto_Flex({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Coffe stores',
@@ -11,10 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <nav className="bg-gray-900 text-white p-4">
+      <body className={`${roboto.className} bg-gray-50`}>
+        <nav className="text-green-700 p-4">
           <div className="container mx-auto">
-            <h1 className="text-2xl font-semibold">Coffee Stores</h1>
+            <h1 className="text-2xl font-black">Coffee Stores</h1>
           </div>
         </nav>
         {children}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
-export default function Home() {
+import { placeSearch } from 'lib/foursquare'
+
+export default async function Home() {
+  const data = await placeSearch();
+
+  console.log(data)
   return (
-    <h1>
-      hello world!
-    </h1>
+    <>
+      <header className="text-orange-700 py-6">
+        <div className="container mx-auto text-center">
+          <h1 className="text-4xl font-black uppercase md:text-6xl">Coffee Discover!</h1>
+          <h2 className="mt-4 text-xl capitalize mt-0 mx-2 ">Find the best coffee stores near you and explore a world of caffeine delights.</h2>
+        </div>
+      </header>
+      
+    </>
   )
 }
