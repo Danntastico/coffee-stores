a5c4aa88d6ada2303b6d1f57d0f78d460ee25a10
Dann <danilo.pena@ascential.com>
2023-09-27 19:39:29 -0500

    initial foursquare configuration

--- app/page.js
+++ app/page.js
@@ -1,18 +1,22 @@
-import { placeSearch } from 'lib/foursquare'
+import { getListOfCoffeeStorePhotos } from '@/lib/unsplash'
+import { fetchStoresByPlace } from 'lib/foursquare'
 
 export default async function Home() {
-  const data = await placeSearch();
+ // const data = await fetchStoresByPlace();
+
+  const data = await getListOfCoffeeStorePhotos()
 
   console.log(data)
   return (
     <>
       <header className="text-orange-700 py-6">
         <div className="container mx-auto text-center">
-          <h1 className="text-4xl font-black uppercase md:text-6xl">Coffee Discover!</h1>
+          <h1 className="text-4xl font-black uppercase md:text-6xl">
+            <span className='text-green-700'>Coffee </span>Discover!
+          </h1>
           <h2 className="mt-4 text-xl capitalize mt-0 mx-2 ">Find the best coffee stores near you and explore a world of caffeine delights.</h2>
         </div>
       </header>
-      
     </>
   )
 }
