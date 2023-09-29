4cc494e1e3cc3d357e8aa68172489aea324300c3
Dann <danilo.pena@ascential.com>
2023-09-28 19:50:48 -0500

    layout background

--- app/layout.js
+++ app/layout.js
@@ -13,13 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} bg-gray-50 m-4`}>
-        <nav className="text-green-700 p-4 border-b-2 border-gray-100">
+      <body className={`${roboto.className} bg-gray-100`}>
+        <nav className="text-green-700 p-4 border-b-2 border-gray-200">
           <div className="container mx-auto">
             <h1 className="text-2xl font-black">Coffee Stores<span className='text-orange-700'>.</span></h1>
           </div>
         </nav>
-        {children}
+        <div className='m4'>
+          {children}
+        </div>
       </body>
     </html>
   )
